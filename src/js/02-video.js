e000428b7cea0103ae413eddfeab01aa30a43c94
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-07-09 13:54:28 +0300

    videoPlaya

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+const _ = require('lodash');
 
+const iFrame = document.querySelector('#vimeo-player');
+const player = new Player(iFrame);
+
+player.on('play', function () {
+  console.log('Playing');
+});
+
+player.on('pause', () => {
+  console.log('Paused');
+});
+
+function timerUpd(data) {
+  // console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', _.throttle(timerUpd, 1000));
+
+const time = localStorage.getItem('videoplayer-current-time') || 0;
+// console.log(time);
+
+player.setCurrentTime(time);
